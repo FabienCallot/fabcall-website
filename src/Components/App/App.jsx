f2cec12d609a0f2e8be82357becbdb9a57d65cb9
FabienCallot <fab.callot@gmail.com>
2022-07-04 15:36:06 +0200

    add react router dom, component HOME, style / vars

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,9 +1,13 @@
+import Home from '../Home/Home';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+  return <div className="home">Coucou</div>;
+};
+
+export default Home;
