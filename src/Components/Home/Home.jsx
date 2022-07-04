2d0b4483aeb2e722117044d9c432c3eb5022f700
FabienCallot <fab.callot@gmail.com>
2022-07-04 17:43:33 +0200

    last modification navbar v1 mobile first

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './home.scss';
 
 const Home = () => {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './Components/App/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './styles/index.scss';
 
 const container = document.getElementById('root');
