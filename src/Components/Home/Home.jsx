efe03e68da4b8b182611a4a444b498cc90e35fd6
FabienCallot <fab.callot@gmail.com>
2022-07-04 15:50:09 +0200

    final commit init project

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './home.scss';
 
 const Home = () => {
-  return <div className="home">Coucou</div>;
+  return <div className="home">Fab.</div>;
 };
 
 export default Home;
