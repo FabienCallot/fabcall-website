b682a6ec34c1565c985e560816e4fc37d0b01b5d
FabienCallot <fab.callot@gmail.com>
2022-07-06 14:38:32 +0200

    adjust style of header for mobile, v1 header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
         <div className="header-button">
           <HashLink
             smooth
-            to="/#contact"
+            to="/#"
             className={
               !isActiveCv
                 ? 'header-button-cv'
