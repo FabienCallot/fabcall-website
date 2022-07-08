038d640636a0a8c6c663417edeb0ddd285312df8
FabienCallot <fab.callot@gmail.com>
2022-07-08 10:23:17 +0200

    add img, text, Button, style to AboutMe and propTypes for button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './button.scss';
 
-const Button = ({ clickEvent, text, className }) => {
+const Button = ({ className, clickEvent, text }) => {
   return (
     <button className={`button ${className}`} onClick={clickEvent}>
       {text}
     </button>
   );
+};
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  clickEvent: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default React.memo(Button);
