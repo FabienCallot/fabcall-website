7b73a44fa8aa8170ac1657cc7e2d277597164a22
FabienCallot <fab.callot@gmail.com>
2022-07-08 17:21:34 +0200

    implementation of card in recent work and style of card. fix line height in button component style

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './card.scss';
 
-const Card = ({ title, img, alt, path, buttonText }) => {
+const Card = ({ className, title, imgSrc, alt, path, buttonText }) => {
   const navigate = useNavigate();
   return (
-    <div className="card">
-      <h3>{title}</h3>
-      <img src={img} alt={alt} />
+    <div className={`card ${className}`}>
+      <h3 className="card-title">{title}</h3>
+      <img src={imgSrc} alt={alt} />
       <Button
         className="card-button"
         clickEvent={() => {
@@ -17,6 +19,15 @@ const Card = ({ title, img, alt, path, buttonText }) => {
       />
     </div>
   );
+};
+
+Card.propTypes = {
+  className: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  buttonText: PropTypes.string.isRequired,
 };
 
 export default Card;
--- src/Components/RecentWorks/RecentWorks.jsx
+++ src/Components/RecentWorks/RecentWorks.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Card from '../Card/Card';
 import Titles from '../Titles/Titles';
+import Fabien from '../../Docs/portrait.png';
 
 const RecentWorks = () => {
   return (
-    <section className="recent-works">
+    <section id="portfolio" className="recent-works">
       <Titles
         className="recent-works-title"
         firstTitle="Recent Works"
         secondTitle={'My Portfolio'}
+      />
+      <Card
+        className="recent-works-card"
+        title="Pokedex"
+        imgSrc={Fabien}
+        alt="home page Pokedex"
+        buttonText="view More"
       />
     </section>
   );
